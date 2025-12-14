a7edbb6568e3cd31521bb1537b56ae0c77b59489
littlebiv <amirarbiv1@mail.tau.ac.il>
2025-12-14 17:55:47 +0200

    time complexity

--- src/AVLTree.py
+++ src/AVLTree.py
@@ -63,13 +63,14 @@ class AVLTree(object):
 	@returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
 	and e is the number of edges on the path between the starting node and ending node+1.
 	"""
-	def search(self, key):
+	def search(self, key): #time complexity O(log n)
 		if self.root is None:
 			return None, -1
 		return self.search_from_node(key, self.root)
 		
 
-	def search_from_node(self, key, start_node): #helping func for search
+	def search_from_node(self, key, start_node): #time complexity O(log n)
+    #helping func for search
 		count = 0
 		curr = start_node
 		while curr.key != key: #regular BST search
@@ -95,7 +96,7 @@ class AVLTree(object):
 	@returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
 	and e is the number of edges on the path between the starting node and ending node+1.
 	"""
-	def finger_search(self, key): 
+	def finger_search(self, key): #time complexity O(log n)
 		if self.root is None: #check if tree is empty
 			return None, -1
 		#if key > self.max_node.key or key < self.min_node.key: #check if key is out of bounds
@@ -127,7 +128,7 @@ class AVLTree(object):
 	e is the number of edges on the path between the starting node and new node before rebalancing,
 	and h is the number of PROMOTE cases during the AVL rebalancing
 	"""
-	def insert(self, key, val):
+	def insert(self, key, val): #time complexity O(log n)
 		return self.insert_from_node(key, val, self.root)
 
 
@@ -147,7 +148,8 @@ class AVLTree(object):
 	and h is the number of PROMOTE cases during the AVL rebalancing
 """
 
-	def insert_from_node(self, key, val, start_node): #helping func for insertions
+	def insert_from_node(self, key, val, start_node): #time complexity O(log n)
+    #helping func for insertions
 		edges = 0
 		new_node = AVLNode(key, val)
 		curr = start_node
@@ -186,7 +188,7 @@ class AVLTree(object):
 	@type node: AVLNode
 	@param node: the newly inserted node
 	"""
-	def update_heights(self, node):
+	def update_heights(self, node): #time complexity O(log n)
 		curr = node.parent
 		while curr is not None:
 			curr.height = 1 + max(curr.right.height, curr.left.height)
@@ -200,7 +202,7 @@ class AVLTree(object):
 	@rtype: int
 	@returns: the balance factor of the node
 	"""
-	def get_bf(self, node):
+	def get_bf(self, node): #time complexity O(1)
 		if not node.is_real_node():
 			return 0
 		return node.left.height - node.right.height
@@ -215,7 +217,8 @@ class AVLTree(object):
 	@returns: number of rotations performed during rebalancing
 	"""
 
-	def rebalance_after_insert(self, key, node): #rebalancing and updating heights going up from the inserted node
+	def rebalance_after_insert(self, key, node): #time complexity O(log n)
+    #rebalancing and updating heights going up from the inserted node
 		curr = node.parent
 		rotations = 0
 		while curr is not None:
@@ -258,7 +261,7 @@ class AVLTree(object):
 	and h is the number of PROMOTE cases during the AVL rebalancing
 	"""
 
-	def finger_insert(self, key, val):
+	def finger_insert(self, key, val): #time complexity O(log n)
 		if self.root is None: #check if tree is empty
 			new_node = AVLNode(key, val)
 			self.root = new_node
@@ -283,7 +286,8 @@ class AVLTree(object):
 	@type node: AVLNode
 	@param node: the unbalanced node
 	"""
-	def rotate_left(self, node): #node is the unbalanced node 
+	def rotate_left(self, node): #time complexity O(1)
+    #node is the unbalanced node 
 		temp = node
 		node = node.right
 		node.parent = temp.parent
@@ -303,7 +307,8 @@ class AVLTree(object):
 		self.update_heights_after_rotation(node)#switch heights
 		return None
 
-	def rotate_right(self, node): #code is similar to rotate_left
+	def rotate_right(self, node): #time complexity O(1)
+    #code is similar to rotate_left
 		temp = node
 		node = node.left
 		node.parent = temp.parent
@@ -325,7 +330,8 @@ class AVLTree(object):
 
 
 
-	def update_heights_after_rotation(self, node): #updating heights after rotation using update_heights
+	def update_heights_after_rotation(self, node): #time complexity O(1)
+    #updating heights after rotation using update_heights
 		if node.right.is_real_node():
 			node.right.height = 1 + max(node.right.left.height, node.right.right.height)
 		if node.left.is_real_node():
@@ -391,7 +397,7 @@ class AVLTree(object):
 	@pre: all keys in self are smaller than key and all keys in tree2 are larger than key,
 	or the opposite way
 	"""
-	def join(self, tree2, key, val):
+	def join(self, tree2, key, val): #time complexity O(log n)
 		#handle edge cases
 		if tree2.root is None and self.root is None: #both trees are empty
 			new_node = AVLNode(key, val)
@@ -527,12 +533,14 @@ class AVLTree(object):
 	dictionary smaller than node.key, and right is an AVLTree representing the keys in the 
 	dictionary larger than node.key.
 	"""
-	def split(self, node): #split using join and delete recursively
+	def split(self, node): #time complexity O(log n)
+    #split using join and delete recursively
 		return self.split_rec(self.root, node.key)
 
 			
 
-	def split_rec(self, node, key): #helper function for split
+	def split_rec(self, node, key): #time complexity O(log n)
+    #helper function for split
 		if not node.is_real_node():
 			return AVLTree(), AVLTree()
 		if node.key < key:
@@ -594,7 +602,8 @@ class AVLTree(object):
 	@rtype: list
 	@returns: a sorted list according to key of touples (key, value) representing the data structure
 	"""
-	def avl_to_array(self): #recursive inorder traversal
+	def avl_to_array(self): #time complexity O(n)
+    #recursive inorder traversal
 		if self.root is None: #check if tree is empty
 			return []
 		res = []
@@ -612,7 +621,7 @@ class AVLTree(object):
 	@rtype: AVLNode
 	@returns: the maximal node, None if the dictionary is empty
 	"""
-	def max_node(self):
+	def max_node(self): #time complexity O(log n)
 		if self.root is None: #check if tree is empty
 			return None
 		curr = self.root
@@ -625,7 +634,7 @@ class AVLTree(object):
 	@rtype: AVLNode
 	@returns: the minimal node, None if the dictionary is empty
 	"""
-	def min_node(self):
+	def min_node(self): #time complexity O(log n)
 		if self.root is None: #check if tree is empty
 			return None
 		curr = self.root
@@ -638,7 +647,8 @@ class AVLTree(object):
 	@rtype: int
 	@returns: the number of items in dictionary 
 	"""
-	def size(self): #simple
+	def size(self): #time complexity O(1)
+    #simple
 		return self._size	
 
 
@@ -647,7 +657,8 @@ class AVLTree(object):
 	@rtype: AVLNode
 	@returns: the root, None if the dictionary is empty
 	"""
-	def get_root(self): #simple
+	def get_root(self): #time complexity O(1)
+    #simple
 		return self.root #None if empty
 
 #create tests for all functions
